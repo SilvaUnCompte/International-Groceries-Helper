d568c0f7562e268852a0db4b2c0142198ee32943
Silvathor <silvablocks@gmail.com>
2025-07-20 18:06:43 +0200

    Add cookie management functions and translations for budget and language settings

---
+++ scripts/translations.js
@@ -0,0 +1,25 @@
+
+export const translations = {
+    fr: {
+        title: "Calculatrice Courses",
+        noEntries: "Aucune entrée",
+        budgetExcess: "Dépassement:",
+        primaryCurrency: "Devise initiale",
+        secondaryCurrency: "Devise à convertir",
+        budgetLimit: "Valeur plafond",
+        language: "Langue",
+        settings: "Paramètres",
+        rotateDevice: "Veuillez tourner votre appareil en mode portrait",
+    },
+    en: {
+        title: "Shopping Calculator",
+        noEntries: "No entries",
+        budgetExcess: "Over budget:",
+        primaryCurrency: "Primary currency",
+        secondaryCurrency: "Convert to currency",
+        budgetLimit: "Budget limit",
+        language: "Language",
+        settings: "Settings",
+        rotateDevice: "Please rotate your device to portrait mode",
+    },
+}
